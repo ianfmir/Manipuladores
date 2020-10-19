3cfcdfb79b60d3e24a5daf32e7cff1accfd983c8
ian <50590634+ianfmir@users.noreply.github.com>
2020-10-19 17:16:40 -0300

    Update moverobo.m

--- moverobo.m
+++ moverobo.m
@@ -6,8 +6,22 @@ clc
 Cen=Cenario([]);
 %Cria um robo
 R = Robo.Cria_KukaKR5();
+%Encontra posicao para o pincel
+MTHC = R.cinematicadir(R.q,'efetuador')*Robo.rot('x',pi/2)*Robo.desl([0;0.05;-0.1]);
+%Parametros do pincel
+raio = 0.05;
+altura = 0.2;
+cor = [1 0 0];
+densidade = 1;
+%Cria pincel
+pincel = Cilindro(MTHC, raio, altura, cor, densidade);
+pincel.desenha();
+R.grudaobjeto(pincel);
 %Adiciona o robo
 Cen.adicionaobjetos(R);
+Cen.adicionaobjetos(pincel);
+Cen.desenha();
+drawnow;
 %Faz a animaçao
 dt=0.01;
 for i = 1: 100
