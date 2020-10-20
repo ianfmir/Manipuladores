a8482365a17d984687d2cc9cc9cb7adfcb10eac1
ian <50590634+ianfmir@users.noreply.github.com>
2020-10-19 21:29:32 -0300

    Update pinta_parede.m

--- pinta_parede.m
+++ pinta_parede.m
@@ -14,7 +14,7 @@ clc
 %% Criação do cenário
 
 % Posicionamento do centro da parede
-MTH=Robo.desl([0;0.7;0.4]);
+MTH=Robo.desl([0;0.75;0.4]);
 % Dimensionamento da parede
 lados = [3 0.1 0.8];
 % Coloração da parede (RGB)
@@ -25,7 +25,7 @@ densidade = 1;
 P=Paralelepipedo(MTH,lados,cor,densidade);
 
 % Posicionamento do centro da janela
-MTHJ=Robo.desl([0;0.7;0.3]);
+MTHJ=Robo.desl([0;0.73;0.3]);
 % Dimensionamento da janela
 ladosJ = [1 0.1 0.6];
 % Coloração da janela (RGB)
@@ -36,6 +36,18 @@ J=Paralelepipedo(MTHJ,ladosJ,corJ,densidade);
 % Criação do robô
 R = Robo.Cria_KukaKR5();
 
+%Encontra posicao para o pincel
+MTHC = R.cinematicadir(R.q,'efetuador')*Robo.rot('x',pi/2)*Robo.desl([0;0.05;-0.1]);
+%Parametros do pincel
+raio = 0.05;
+altura = 0.2;
+cor = [1 0 0];
+densidade = 1;
+%Cria pincel
+pincel = Cilindro(MTHC, raio, altura, cor, densidade);
+pincel.desenha();
+R.grudaobjeto(pincel);
+
 % Criação do cenário
 C = Cenario(R);
 % Adição da parede
@@ -44,10 +56,11 @@ C.adicionaobjetos(P);
 C.adicionaobjetos(J);
 % Adição do robô
 C.adicionaobjetos(R);
+C.adicionaobjetos(pincel);
 
 % Criação da posição inicial desejada do efetuador
 Tef0= R.cinematicadir(R.q, 'efetuador');
-Tdes = Robo.desl([-1.5; 0.6; 0]); %*Tef0
+Tdes = Robo.desl([-1.4; 0.6; 0])*Robo.rot('y',-pi/2)*Robo.rot('x',-pi/2);
 xdes = Tdes(1:3,1);
 ydes = Tdes(1:3,2);
 zdes = Tdes(1:3,3);
@@ -62,31 +75,37 @@ C.desenha();
 
 pause();
 
-%% Movimentação da posição desejada do efetuador
-% Percorre o eixo X da parede
-for x = 0:60
-    % Novo valor para o X desejado
-    newxdes = -1.5+x/20;
-    % Percorre o eixo Y da parede
-    for z = 0:80
-        % Novo valor para o Z desejado
-        newzdes = z/100;
-        % Verificando se a posição em questão não é a janela
-        if ((newxdes < -0.5 || newxdes > 0.5) || newzdes > 0.6)
-            % Tdes assume novos valores
-            Tdes = Robo.desl([newxdes; 0.6; newzdes]);
-            % Criação do eixo da posição desejada com os novos valores
-            E = Eixo(Tdes, 0.2,{'xdes', 'ydes', 'zdes'});
-            % As próximas três linhas devem ser substituídas pelo movimento
-            % do robô, mas neste momento estão aqui para mostrar na figura
-            % que o frame desejado percorre toda a parede, com exceção da
-            % porta.
-            C.adicionaobjetos(E);
-            C.desenha();
-            drawnow;
-        end
-    end
-end
+% %% Movimentação da posição desejada do efetuador
+% % Percorre o eixo X da parede
+% for x = 0:60
+%     % Novo valor para o X desejado
+%     newxdes = -1.5+x/20;
+%     % Percorre o eixo Y da parede
+%     for z = 0:80
+%         % Novo valor para o Z desejado
+%         newzdes = z/100;
+%         % Verificando se a posição em questão não é a janela
+%         if ((newxdes < -0.5 || newxdes > 0.5) || newzdes > 0.6)
+%             % Tdes assume novos valores
+%             Tdes = Robo.desl([newxdes; 0.6; newzdes]);
+%             % Criação do eixo da posição desejada com os novos valores
+%             E = Eixo(Tdes, 0.2,{'xdes', 'ydes', 'zdes'});
+%             % As próximas três linhas devem ser substituídas pelo movimento
+%             % do robô, mas neste momento estão aqui para mostrar na figura
+%             % que o frame desejado percorre toda a parede, com exceção da
+%             % porta.
+%             
+%             for i = 1: 100
+%                 %Atualiza a mth do Robo
+%                 MTH=Robo.desl([-0.01*i;0;0]);
+%                 R.mth = MTH;
+%                 R.config(R.q);
+%                 C.desenha();
+%                 drawnow;
+%             end
+%         end
+%     end
+% end
 %% Movimentação do robô
 
 % Constantes
@@ -99,43 +118,102 @@ r_hist=[];
 u_hist=[];
 t=[];
 
-for k = 1: 2/dt %tempo em s/dt
-    q_hist(:,k) = R.q;
-    t(k) = (k-1)*dt;
-    
-    Tef = R.cinematicadir(R.q,'efetuador');
-    Jgeo = R.jacobianageo(R.q,'efetuador');
-    
-    pef = Tef(1:3,4);
-    xef = Tef(1:3,1);
-    yef = Tef(1:3,2);
-    zef = Tef(1:3,3);
-    
-    Jp = Jgeo(1:3,:);
-    Jw = Jgeo(4:6,:);
-    
-    rpos = pef-pdes;
-    rorix = 1-xdes'*xef;
-    roriy = 1-ydes'*yef;
-    roriz = 1-zdes'*zef;
-    
-    r = [rpos;rorix;roriy;roriz];
-    
-    Jrpos = Jp;
-    Jrorix = xdes'*Robo.matrizprodv(xef)*Jw;  
-    Jroriy = ydes'*Robo.matrizprodv(yef)*Jw;
-    Jroriz = zdes'*Robo.matrizprodv(zef)*Jw;
-    
-    Jr = [Jrpos;Jrorix;Jroriy;Jroriz];
-    
-    u = Robo.pinvam(Jr,0.001)*(-K*r);
-    
-    u_hist(:,k) = u;
-    r_hist(:,k) = r;
-    
-    qprox = R.q + u*dt;
-    R.config(qprox);
+%movimenta base do robo para x=-1.2
+for i = 1: 120
+    %Atualiza a mth do Robo
+    MTH=Robo.desl([-0.01*i;0;0]);
+    R.mth = MTH;
+    R.config(R.q);
     C.desenha();
     drawnow;
-    
 end
+
+
+%posiciona o pincel na parte inferior esquerda da parede
+for k = 0:6
+    Tdes = Tdes*Robo.desl([0.1*k;0;0]);
+    xdes = Tdes(1:3,1);
+    ydes = Tdes(1:3,2);
+    zdes = Tdes(1:3,3);
+    pdes = Tdes(1:3,4);
+    for k = 1: 2/dt %tempo em s/dt
+        q_hist(:,k) = R.q;
+        t(k) = (k-1)*dt;
+
+        Tef = R.cinematicadir(R.q,'efetuador');
+        Jgeo = R.jacobianageo(R.q,'efetuador');
+
+        pef = Tef(1:3,4);
+        xef = Tef(1:3,1);
+        yef = Tef(1:3,2);
+        zef = Tef(1:3,3);
+
+        Jp = Jgeo(1:3,:);
+        Jw = Jgeo(4:6,:);
+
+        rpos = pef-pdes;
+        rorix = 1-xdes'*xef;
+        roriy = 1-ydes'*yef;
+        roriz = 1-zdes'*zef;
+
+        r = [rpos;rorix;roriy;roriz];
+
+        Jrpos = Jp;
+        Jrorix = xdes'*Robo.matrizprodv(xef)*Jw;  
+        Jroriy = ydes'*Robo.matrizprodv(yef)*Jw;
+        Jroriz = zdes'*Robo.matrizprodv(zef)*Jw;
+
+        Jr = [Jrpos;Jrorix;Jroriy;Jroriz];
+
+        u = Robo.pinvam(Jr,0.001)*(-K*r);
+
+        u_hist(:,k) = u;
+        r_hist(:,k) = r;
+
+        qprox = R.q + u*dt;
+        R.config(qprox);
+        C.desenha();
+        drawnow;    
+    end
+end
+
+
+% %movimenta base do robo para x=-0.8
+% for i = 1: 40
+%     %Atualiza a mth do Robo
+%     MTH=Robo.desl([0.01*i;0;0]);
+%     R.mth = MTH;
+%     R.config(R.q);
+%     C.desenha();
+%     drawnow;
+% end
+% 
+% %movimenta base do robo para x= 0 
+% for i = 1: 80
+%     %Atualiza a mth do Robo
+%     MTH=Robo.desl([0.01*i;0;0]);
+%     R.mth = MTH;
+%     R.config(R.q);
+%     C.desenha();
+%     drawnow;
+% end
+% 
+% %movimenta base do robo para x= 0.8
+% for i = 1: 80
+%     %Atualiza a mth do Robo
+%     MTH=Robo.desl([0.01*i;0;0]);
+%     R.mth = MTH;
+%     R.config(R.q);
+%     C.desenha();
+%     drawnow;
+% end
+% 
+% %movimenta base do robo para x= 1.2
+% for i = 1: 40
+%     %Atualiza a mth do Robo
+%     MTH=Robo.desl([0.01*i;0;0]);
+%     R.mth = MTH;
+%     R.config(R.q);
+%     C.desenha();
+%     drawnow;
+% end
